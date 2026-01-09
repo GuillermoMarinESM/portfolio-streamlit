75cc2a4a90e5496356be94bab6477d6ef6390dab
GuillermoMarinESM <96889440+GuillermoMarinESM@users.noreply.github.com>
2026-01-09 00:15:05 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(page_title="Data Scientist Portfolio",
+                   layout = "wide")
+st.title("Guillermo Marin - Data Scientist")
+
+st.write("""
+Mathematician & Data Scientist specialized in forecasting,
+statistical modeling, and machine learning
+""")
+st.info("Use the sidebar to navigate through projects.")
+
